6f296f5b7f111b22dc50805896530d82c1d67f9e
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-07 12:32:39 +0000

    Alterado 12:32

--- gitauto.sh
+++ gitauto.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 HORA=$(date +%H)
-
 MIN=$(date +%M)
 
 $(git add .)
 sleep 3
-$(git commit -a -m 'Alterado $HORA:$MIN')
+$(git commit -a -m "Alterado $HORA:$MIN")
 sleep 3
 $(git push)
