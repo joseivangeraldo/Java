afdee3d80e2e4f3ee497315cfda3cf51b459fddc
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-07 13:53:44 +0000

    date

--- work/First.java
+++ work/First.java
@@ -1,9 +1,11 @@
 import java.lang.*;
 
-class First
+public class First
 {
-    public static void main(String arg[])
+    public static void main(String[] args)
     {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");// exibir um texto com quebra de linha
+        System.out.println(args[0]);// exibir texto apos linnha de comando
+        System.out.println(args[1]);// exibir texto 2 apos linha de comando
     }
 }
