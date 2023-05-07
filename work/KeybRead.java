d6e865616d2f9d2b586f6757f17348e6066fb191
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-07 14:17:26 +0000

    date

---
+++ work/KeybRead.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+
+class KeybRead {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int a, b, c;
+
+        // LER ENTRADAS INT
+        System.out.println("Enter 2 numbers");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = a + b;
+        System.out.println("Sum is " + c);
+
+        // LER ENTRADAS STRING
+        Scanner w = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = w.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
