081827feb3f1e875473269bed73239fe54c9a325
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-08 20:53:05 +0000

    08/05/2023 20:53:02

--- work/DataSizeRange.java
+++ work/DataSizeRange.java
@@ -4,8 +4,9 @@ class DataSizeRange
 {
     public static void main(String arg[])
     {
-        int x;
-        Integer y;
+        System.out.println("Int Min:" + Integer.MIN_VALUE);
+        System.out.println("Int Max:" + Integer.MAX_VALUE);
+        System.out.println("Int Bytes " + Integer.BYTES);
 
     }
 }
