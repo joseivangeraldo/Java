71d8d9a8f91f7b749288260f18413c67ed7000f3
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-09 00:50:55 +0000

    09/05/2023 00:50:52

---
+++ work/Variables.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+class Variables
+{
+    public static void main(String arg[])
+    {
+        byte b = 127;// nao permite numero maior que 127
+        short s =300;
+        int i = 100;
+
+        float f= 25.3f;
+        char c ='A';
+
+        System.out.println(b);   
+        System.out.println(s);   
+        System.out.println(i);   
+        System.out.println(f);  
+        System.out.println(c);   
+ 
+
+
+    }
+}
