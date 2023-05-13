3f5e21c7c7d2b17118559e11702fc69953963c9c
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-13 01:04:09 +0000

    13/05/2023 01:04:06

---
+++ work/Arithmetic.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        char a = 40;
+        int b = 30;
+
+        int c = a-b;
+
+
+        
+        System.out.println(c);
+
+    }
+}
