6f29a3986cd39d5ee22af119dc5950f43ef39a49
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-15 12:10:51 +0000

    15/05/2023 12:10:48

---
+++ work/Expression3.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class Expression3
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+
+        int a,b,c;
+        double r1,r2;
+
+        System.out.println("Enter values a, b, and c ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+
+        r1 = (-b+Math.sqrt(b*b - 4*a*c))/(2*a);
+        r2 = (-b-Math.sqrt(b*b - 4*a*c))/(2*a);
+
+        System.out.println("Root are "+r1 + " "+ r2);
+
+    }
+}
