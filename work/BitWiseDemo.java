789ccdca614338f13ab8f3d0564d65d1bebdd0ef
José Ivan Geraldo Silva <ivan.geraldo@gmail.com>
2023-05-19 23:06:01 +0000

    19/05/2023 23:05:58

---
+++ work/BitWiseDemo.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class BitWiseDemo
+{
+    public static void main(String[] args)
+    {
+        int x=0b1010,y=0b0110,z;
+        z= x|y;
+
+        System.out.println(z);
+    }
+}
