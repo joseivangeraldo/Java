0c05022c4e36d05a1d7d6293b987d851ff677b41
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-22 13:31:53 +0000

    22/05/2023 13:31:50

--- work/BitWiseDemo.java
+++ work/BitWiseDemo.java
@@ -4,9 +4,13 @@ class BitWiseDemo
 {
     public static void main(String[] args)
     {
-        int x=0b1010,y=0b0110,z;
-        z= x|y;
+        int x=-0b1010;
+        int y;
+        y = ~x;
 
-        System.out.println(z);
+        System.out.println(String.format("%s",Integer.toBinaryString(x)));
+        System.out.println(String.format("%32s",Integer.toBinaryString(y)));
+        System.out.println(y);
+
     }
 }
