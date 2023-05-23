8526ce8bedd3b6b3d5f618d98a6173a1bc764e49
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-23 12:24:37 +0000

    23/05/2023 12:24:34

---
+++ work/BitWise.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+/* Poderia fazer sem usar variaveis simplesmente digitando  o numero direto na 
+operação de shift bits:
+byte c;
+
+c=(byte)(a<<9);
+c=(byte)(c|12); */
+
+class BitWise
+{
+    public static void main(String args[]) 
+    {
+        byte a=9, b=12;
+
+        byte c;
+
+        c=(byte)(a<<4);
+        c=(byte)(c|b);
+
+       System.out.println((c&0b11110000)>>4);
+       System.out.println((c&0b00001111));
+    }    
+
+}
---
+++ work/WideNarroDemo.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+public class WideNarroDemo
+{
+    public static void main(String args[])
+    {
+        byte b=10;
+        short s=10;
+        int i=10;
+        long l=10;
+        float f=10;
+        double d=10;
+        char c=10;
+        boolean bl=true;
+
+        b=s;
+
+    }
+}
