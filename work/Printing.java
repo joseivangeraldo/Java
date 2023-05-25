199d6de3a5515d510f7d05b553fdaeb56d26371b
José Ivan Geraldo Silva <ivan.geraldo@gmail.com>
2023-05-25 20:57:37 +0000

    25/05/2023 20:57:34

---
+++ work/Printing.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Printing
+{
+    public static void main(String args[])
+    {
+        int x=10, y=20;
+
+        System.out.println("Sum "+ (x+y));  
+        System.out.println("Sum "+x+ " and "+ y + " is " + (x+y));  
+
+    }
+}                   
