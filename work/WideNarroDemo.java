41cacab4423b79c942aef993719bf5c6f1312a78
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-05-25 20:24:07 +0000

    25/05/2023 20:24:04

--- work/WideNarroDemo.java
+++ work/WideNarroDemo.java
@@ -5,15 +5,23 @@ public class WideNarroDemo
     public static void main(String args[])
     {
         byte b=10;
-        short s=10;
+        short s=120;
         int i=10;
         long l=10;
-        float f=10;
+
+        float f=10.5f;
+
         double d=10;
         char c=10;
         boolean bl=true;
 
-        b=s;
+        b=(byte)s;
+        System.out.println(b);
+
+        i=(int)f;
+        System.out.println(i);
+
+
 
     }
 }
