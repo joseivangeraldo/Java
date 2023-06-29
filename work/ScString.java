2e2c4562fa899fbc7828f810a9a3d6ca8c13ce16
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-06-29 00:00:07 +0000

    29/06/2023 00:00:04

---
+++ work/ScString.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class ScString
+{
+    public static void main(String[] args)
+    {
+        String str="programmer@gmail.com";
+
+        int i=str.indexOf("@");
+        String uname=str.substring(i+1,str.lenght());
+        System.out.println();
+        
+    }
+}
